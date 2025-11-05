333656fb85c56f8b6d87a54e5f0545d2c644efde
LyndseyUni <thrly001@mymail.unisa.edu.au>
2025-11-05 18:50:04 +1030

    Created enclosure class added data validation and exception handling and added private attributes

--- enclosure.py
+++ enclosure.py
@@ -6,3 +6,31 @@ ID: Thrly001@mymail.unisa.edu.au
 Username: thrly001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+# Import Animal class
+
+from animal import Animal
+
+# Create the Enclosure class
+
+class Enclosure:
+    def __init__(self, name, enc_type, size):
+
+        # data validation and exception handling
+
+        if not name or not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        if not enc_type or not isinstance (enc_type, str):
+            raise ValueError("Enc_type must be a string")
+        if not isinstance (size, int):
+            raise ValueError("Size must be an integer")
+
+    # Enclosure class private attributes
+
+        self.__name = name
+        self.__enc_type = enc_type
+        self.__cleanliness = "Clean"
+        self.__animals = []
+
+
+
