dfd24cb81c566c3a8b17fe1d1d983e2a2052c560
LyndseyUni <thrly001@mymail.unisa.edu.au>
2025-11-09 18:30:38 +1030

    Started creating the Main function and creating Animal object and Health Issue objects

--- main.py
+++ main.py
@@ -7,3 +7,26 @@ Username: thrly001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+from datetime import date
+from animal import Mammal, Bird, Reptile, HealthIssue
+from enclosure import Aquarium, Aviary, Vivarium
+from staff import Zookeeper, Veterinarian
+
+def main():
+
+    # Creating Animal objects
+
+    simba = Mammal("Simba", "Lion", 5, "Carnivore", "land")
+    rio = Bird("Rio", "Parrot", 3, "Herbivore", "sky")
+    kaa = Reptile("Kaa", "Snake", 5, "Carnivore", "land")
+
+    # Creating health issue objects
+
+    broken_leg = HealthIssue("Broken leg", date.today(), 3, "Rest 6 weeks")
+    simba.add_health_issue(broken_leg)
+
+    gastro = HealthIssue("Gastro", date.today(), 3, "Needs meds")
+    rio.add_health_issue(gastro)
+
+    splinter = HealthIssue("Splinter", date.today(), 3, "Extraction")
+    kaa.add_health_issue(splinter)
